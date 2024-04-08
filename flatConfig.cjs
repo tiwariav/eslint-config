dc0f0271890109f88be5ec814e2886d68b6a4378
Arvind Tiwari <tiwariav@gmail.com>
2024-04-09 01:11:18 +0530

    refactor: update max-lines-per-function options

--- flatConfig.cjs
+++ flatConfig.cjs
@@ -191,7 +191,7 @@ module.exports = [
       "logical-assignment-operators": "error",
       "max-depth": "warn",
       "max-lines": "warn",
-      "max-lines-per-function": "warn",
+      "max-lines-per-function": ["warn", { max: 60 }],
       "max-params": "warn",
       "max-statements": "warn",
       "no-alert": "error",
