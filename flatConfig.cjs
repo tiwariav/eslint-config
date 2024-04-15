2d90c1b654acda3ca52a6b573787782bdcc89783
Arvind Tiwari <tiwariav@gmail.com>
2024-04-16 03:58:29 +0530

    refactor: add '2' to list of ignored magic numbers

--- flatConfig.cjs
+++ flatConfig.cjs
@@ -39,7 +39,7 @@ const settings = {
 };
 
 const noMagicNumbersOptions = {
-  ignore: [0, 1, -1],
+  ignore: [-1, 0, 1, 2],
   ignoreArrayIndexes: true,
   ignoreClassFieldInitialValues: true,
   ignoreDefaultValues: true,
